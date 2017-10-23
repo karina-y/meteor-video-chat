4a66638f39290c5770ff9e5069744eb318dd9432
jerp dertin <jack@pebblepad.co.uk>
2017-10-23 13:00:48 +0100

    move server to own index file, further debugging

--- package.js
+++ package.js
@@ -1,24 +1,25 @@
 //jshint esversion: 6
 Package.describe({
-  name: 'elmarti:video-chat',
-  version: '2.0.0',
-  summary: 'Simple WebRTC Video Chat for your app.',
-  git: 'https://github.com/elmarti/meteor-video-chat',
-  documentation: 'README.md'
+    name: 'elmarti:video-chat',
+    version: '2.0.0',
+    summary: 'Simple WebRTC Video Chat for your app.',
+    git: 'https://github.com/elmarti/meteor-video-chat',
+    documentation: 'README.md'
 });
 
 Package.onUse(api => {
 
-  Npm.depends({
-      "core-rtc":"0.0.2-alpha-12"
-  });
+    Npm.depends({
+        "core-rtc": "0.0.2-alpha-15"
+    });
 
 
-  api.versionsFrom('1.5');
-  api.use('ecmascript');
-  api.use("rocketchat:streamer@0.5.0");
-  api.use("mizzao:user-status@0.6.6");
-  api.addFiles(['lib/index.js'], "client");
-  api.addFiles(['lib/publish.js'], "server");
-  api.addFiles(['lib/adapter.js'], "client");
+    api.versionsFrom('1.5');
+    api.use('ecmascript');
+    api.use("rocketchat:streamer@0.5.0");
+    api.use("mizzao:user-status@0.6.6");
+    api.addFiles(['lib/index.js'], "client");
+    api.addFiles(['lib/publish.js'], "server");
+    api.addFiles(['lib/index.server.js'], 'server');
+    api.addFiles(['lib/adapter.js'], "client");
 });
