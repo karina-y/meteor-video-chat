e409115dd8370be21672d60529a61460358a7a0c
jerp dertin <jack@pebblepad.co.uk>
2017-11-15 12:24:54 +0000

    fix travis yml

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 //jshint esversion: 6
 Package.describe({
     name: 'elmarti:video-chat',
-    version: '2.0.0',
+    version: '2.0.0-alpha-1',
     summary: 'Simple WebRTC Video Chat for your app.',
     git: 'https://github.com/elmarti/meteor-video-chat',
     documentation: 'README.md'
