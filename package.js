642e5a7389b3f408016408d40674005447ea15f5
jerp <info@jacobo.tech>
2018-01-23 18:41:39 +0000

    bump rtcfly version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 //jshint esversion: 6
 Package.describe({
     name: 'elmarti:video-chat',
-    version: '2.1.0',
+    version: '2.1.1',
     summary: 'Simple WebRTC Video Chat for your app.',
     git: 'https://github.com/elmarti/meteor-video-chat',
     documentation: 'README.md'
@@ -10,7 +10,7 @@ Package.describe({
 Package.onUse(api => {
 
     Npm.depends({
-        "rtcfly": "0.1.4"
+        "rtcfly": "0.1.5"
     });
 
 
