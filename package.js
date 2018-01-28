3fdcf553c5019348f70aaa89491b40b345f32731
jerp <info@jacobo.tech>
2018-01-28 15:31:22 +0000

    Update dependencies for Meteor 1.6.1

--- package.js
+++ package.js
@@ -16,8 +16,8 @@ Package.onUse(api => {
 
     api.versionsFrom('1.5');
     api.use('ecmascript');
-    api.use("rocketchat:streamer@0.5.0");
-    api.use("mizzao:user-status@0.6.6");
+    api.use("rocketchat:streamer@0.6.1");
+    api.use("mizzao:user-status@0.6.7");
     api.addFiles(['lib/index.js'], "client");
     api.addFiles(['lib/publish.js'], "server");
     api.addFiles(['lib/index.server.js'], 'server');
